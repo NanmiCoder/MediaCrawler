3a8dc573860d13eabcf9980802f0cd546e51e756
Qiuyu Wu <qiuyu.wu@shopee.com>
2023-11-16 18:47:22 +0800

    feat: to api

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import db
 from base import proxy_account_pool
 from media_platform.douyin import DouYinCrawler
 from media_platform.xhs import XiaoHongShuCrawler
+from fastapi import FastAPI, HTTPException, Request
+import asyncio
+
+app = FastAPI()
 
 
 class CrawlerFactory:
@@ -44,13 +48,50 @@ async def main():
     )
     await crawler.start()
 
+# 修改 main 函数以接受参数
 
+
+async def _main(platform: str, login_type: str):
+    account_pool = proxy_account_pool.create_account_pool()
+
+    if config.IS_SAVED_DATABASED:
+        await db.init_db()
+
+    crawler = CrawlerFactory.create_crawler(platform=platform)
+    crawler.init_config(
+        platform=platform,
+        login_type=login_type,
+        account_pool=account_pool
+    )
+    await crawler.start()
+
+
+# 定义 API 路由
+@app.post("/api/v1/search")
+async def search(request: Request):
+    body = await request.json()
+    _ = body.get("user_id")
+    _ = body.get("key_word")
+    _ = body.get("desc")
+
+    # 调用 main 函数
+    platform = "xhs"  # 或从请求中获取
+    login_type = "qrcode"  # 或从请求中获取
+    await _main(platform, login_type)
+
+    return {"message": "Crawler started"}
+
+# 运行服务器
 if __name__ == '__main__':
-    try:
-        # asyncio.run(main())
-        asyncio.get_event_loop().run_until_complete(main())
-    except KeyboardInterrupt:
-        sys.exit()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# if __name__ == '__main__':
+#     try:
+#         # asyncio.run(main())
+#         asyncio.get_event_loop().run_until_complete(main())
+#     except KeyboardInterrupt:
+#         sys.exit()
 
 # TODO:
 # 1. 先把main函数服务化，用api调用main
