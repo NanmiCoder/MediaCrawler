4f5f83d3faaac6de552aadd5a3f188e8883b9fc4
ccgo <>
2024-02-22 00:07:40 +0800

    feat: 增加sqlite配置示例

--- config/db_config.py
+++ config/db_config.py
@@ -7,3 +7,6 @@ REDIS_DB_PWD = os.getenv("REDIS_DB_PWD", "123456")  # your redis password
 # mysql config
 RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "123456")  # your relation db password
 RELATION_DB_URL = f"mysql://root:{RELATION_DB_PWD}@localhost:3306/media_crawler"
+
+# sqlite3 config
+# RELATION_DB_URL = f"sqlite://data/media_crawler.sqlite"
