94d24a9530d62c1b17e880a46b4b6c4fea980722
Nan Zhou <nelzomal@gmail.com>
2024-05-30 15:35:05 +0800

    feat: improve db config

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,21 @@
 import os
+import dotenv
+
+dotenv.load_dotenv()
 
 # redis config
 REDIS_DB_HOST = "127.0.0.1"  # your redis host
 REDIS_DB_PWD = os.getenv("REDIS_DB_PWD", "123456")  # your redis password
 
 # mysql config
-RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "123456")  # your relation db password
-RELATION_DB_URL = f"mysql://root:{RELATION_DB_PWD}@localhost:3306/media_crawler"
+RELATION_DB_PWD = os.getenv("RELATION_DB_PWD", "123456")
+RELATION_DB_USER = os.getenv("RELATION_DB_USER", "root")
+RELATION_DB_HOST = os.getenv("RELATION_DB_HOST", "localhost")
+RELATION_DB_PORT = os.getenv("RELATION_DB_PORT", "3306")
+RELATION_DB_NAME = os.getenv("RELATION_DB_NAME", "media_crawler")
+
+
+RELATION_DB_URL = f"mysql://{RELATION_DB_USER}:{RELATION_DB_PWD}@{RELATION_DB_HOST}:{RELATION_DB_PORT}/{RELATION_DB_NAME}"
 
 # sqlite3 config
 # RELATION_DB_URL = f"sqlite://data/media_crawler.sqlite"
