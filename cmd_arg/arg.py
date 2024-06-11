645ec729f6c059d707da19c8070df6e06c55fa34
Relakkes <relakkes@gmail.com>
2024-06-11 23:55:42 +0800

    fix: command args need default value

--- cmd_arg/arg.py
+++ cmd_arg/arg.py
@@ -1,19 +1,20 @@
 import argparse
 import config
 
+
 async def parse_cmd():
     # 读取command arg
     parser = argparse.ArgumentParser(description='Media crawler program.')
     parser.add_argument('--platform', type=str, help='Media platform select (xhs | dy | ks | bili | wb)',
-                        choices=["xhs", "dy", "ks", "bili", "wb"])
+                        choices=["xhs", "dy", "ks", "bili", "wb"], default=config.PLATFORM)
     parser.add_argument('--lt', type=str, help='Login type (qrcode | phone | cookie)',
-                        choices=["qrcode", "phone", "cookie"])
+                        choices=["qrcode", "phone", "cookie"], default=config.LOGIN_TYPE)
     parser.add_argument('--type', type=str, help='crawler type (search | detail | creator)',
-                        choices=["search", "detail", "creator"])
+                        choices=["search", "detail", "creator"], default=config.CRAWLER_TYPE)
     parser.add_argument('--start', type=int,
-                        help='number of start page')
+                        help='number of start page', default=config.START_PAGE)
     parser.add_argument('--keywords', type=str,
-                        help='please input keywords')
+                        help='please input keywords', default=config.KEYWORDS)
 
     args = parser.parse_args()
 
