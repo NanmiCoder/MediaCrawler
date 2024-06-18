6425b81338830709f9a213eef779d2f133092d68
enlia <2020621056@qq.com>
2024-06-18 13:06:23 +0800

    移动表，融合表，删除表

--- tools/fang.py
+++ tools/fang.py
@@ -13,23 +13,24 @@ conn=pymysql.connect(
     database='media_crawler'
 )   # 连接数据库,database对应数据库
 try:
-    with conn.cursor() as cursor:
-        cursor=conn.cursor()    # 建立游标对象,用来执行各种操作
+    with conn.cursor() as cursor:  # 建立游标对象,用来执行各种操作
         cursor.execute(f"""DROP TABLE IF EXISTS {MERGE}""")    # 保证删除merge表
         cursor.execute(f"""CREATE TABLE {MERGE}(
             `id` int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
             `content` longtext COMMENT '评论内容',
             `add_ts` bigint NOT NULL COMMENT '记录添加时间戳',
+            `last_modify_ts` bigint NOT NULL COMMENT '记录最后修改时间戳',
             PRIMARY KEY (`ID`)
         )""")  # 创建merge表
         for table in tables:    # 将全部评论放到一张表中
-            sql=f"""SELECT `content`,`add_ts` FROM {table}"""
+            cursor.execute(f"""create table fang.{table} as select * from {table}""")   # 复制所哟评论表到fang中
+            sql=f"""SELECT `content`,`add_ts`,`last_modify_ts` FROM {table}"""  # 提取评论和时间戳
             cursor.execute(sql)
             rows = cursor.fetchall()    # 一个大元组,里面是小元组(content,add_ts)
             for row in rows:    # 开始插入数据
                 cursor.execute(f"""INSERT INTO {MERGE} (`content`,`add_ts`) VALUES (%s,%s)""",row)
-        # 清楚原来表中的内容
-        # sql="""truncate table"""
+        # 删除原来表中的内容
+            cursor.execute(f"""truncate table {table}""")
     conn.commit()
 finally:
     conn.close()
